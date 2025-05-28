f1eec9ef7c5382865689d97d27f35a1f83b22cce
Danhnguyen <21130308@st.hcmuaf.edu.vn>
2025-05-29 01:49:07 +0700

    up FE

--- BE/src/main/java/com/example/beprojectweb/config/SecurityConfig.java
+++ BE/src/main/java/com/example/beprojectweb/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
