4c0e982b3bdf506729dad86095c9cae436458717
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2025-08-20 09:33:54 +0700

    fix cors

--- BE/src/main/java/com/example/beprojectweb/config/WebConfig.java
+++ BE/src/main/java/com/example/beprojectweb/config/WebConfig.java
@@ -18,8 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://192.168.1.110:5173")
+                .allowedOrigins(
+                "http://localhost:5173",
+                "http://192.168.1.110:5002"
+            )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
