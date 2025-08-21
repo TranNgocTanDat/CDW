0dd95a5ec766ddb93ef2bcdd6ab51d5f114e74a0
TranNgocTanDat <21130314@st.hcmuaf.edu.vn>
2025-08-21 02:08:52 +0000

    update docker-compose

--- BE/src/main/java/com/example/beprojectweb/config/WebConfig.java
+++ BE/src/main/java/com/example/beprojectweb/config/WebConfig.java
@@ -18,9 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://192.168.1.110:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(
+                "http://192.168.1.110:5002",
+		"http://frontend.dren.io.vn"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE") 
                 .allowCredentials(true);
     }
 }
