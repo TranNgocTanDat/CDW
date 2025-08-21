47396fe2d3e1848c61e35925f116600fb6bb3ab0
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2025-08-21 16:43:47 +0700

    fix spa

--- BE/src/main/java/com/example/beprojectweb/config/WebConfig.java
+++ BE/src/main/java/com/example/beprojectweb/config/WebConfig.java
@@ -19,9 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                "http://localhost:5173",
                 "http://192.168.1.110:5002",
-                "http:frontend.dren.io.vn"
+                "https://frontend.dren.io.vn"
             )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
